d5dd52b17cf31d7f2a82123923695bc581a9cc5a
Talha <atercan0658@gmail.com>
2022-11-11 17:43:49 +0100

    login responsive done

--- src/components/styles/Login.styled.jsx
+++ src/components/styles/Login.styled.jsx
@@ -3,16 +3,41 @@ import { Link } from "react-router-dom";
 
 export const LoginSection = styled.section`
   padding: 2rem 1.5rem;
-  height: calc(100vh - 56px);
+  min-height: calc(100vh - 56px);
+  display: flex;
+  justify-content: center;
+  @media screen and (min-width: 576px) {
+    min-height: calc(100vh - 97px);
+  }
+  @media screen and (min-width: 768px) {
+    min-height: calc(100vh - 103px);
+  }
+`;
+
+export const Form = styled.form`
   display: flex;
   flex-direction: column;
-  justify-content: center;
+  gap: 0.7rem;
+  padding: 2rem 1rem;
+  background-color: #fff;
+  box-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px,
+    rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px,
+    rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;
+  margin: auto;
+  border-radius: 20px;
+  @media screen and (min-width: 768px) {
+    padding: 2rem;
+    width: 500px;
+  }
 `;
 
 export const LoginDiv = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
+  @media screen and (min-width: 768px) {
+    padding: 0 1rem;
+  }
 `;
 
 export const LoginTitle = styled.h3`
@@ -27,21 +52,11 @@ export const LoginText = styled.p`
   font-size: 1rem;
 `;
 
-export const Form = styled.form`
-  display: flex;
-  flex-direction: column;
-  gap: 0.7rem;
-  padding: 2rem 1rem;
-  background-color: #fff;
-  box-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px,
-    rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px,
-    rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;
-  margin: auto;
-  border-radius: 20px;
-`;
-
 export const SignUpLink = styled(Link)`
   color: #000;
+  &:hover {
+    opacity: 0.7;
+  }
 `;
 
 export const FlexCol = styled.div`
@@ -54,4 +69,8 @@ export const LoginLink = styled.button`
   text-decoration: underline;
   border: none;
   color: #000;
+  cursor: pointer;
+  &:hover {
+    opacity: 0.8;
+  }
 `;
